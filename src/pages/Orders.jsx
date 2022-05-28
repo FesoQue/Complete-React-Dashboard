5331ad2247a1062c30931df850a32d31bf50f24f
FesoQue <adefesoq@gmail.com>
2022-05-28 04:59:17 +0100

    ecomm & orders page completed. employees page wip

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -1,9 +1,54 @@
-import React from 'react'
+import React from 'react';
+import {
+  GridComponent,
+  ColumnsDirective,
+  ColumnDirective,
+  Resize,
+  Sort,
+  ContextMenu,
+  Filter,
+  Page,
+  ExcelExport,
+  PdfExport,
+  Edit,
+  Inject,
+} from '@syncfusion/ej2-react-grids';
+import { ordersData, contextMenuItems, ordersGrid } from '../asset/dummy';
+import Header from '../component/Header';
 
 const Orders = () => {
   return (
-    <div>Orders</div>
-  )
-}
+    <div className='m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl'>
+      <Header category='Page' title='Orders' />
+      <GridComponent
+        id='gridcomp'
+        dataSource={ordersData}
+        allowPaging
+        allowSorting
+        allowExcelExport
+        allowPdfExport
+        contextMenuItems={contextMenuItems}
+      >
+        <ColumnsDirective>
+          {ordersGrid.map((item, index) => (
+            <ColumnDirective key={index} {...item} />
+          ))}
+        </ColumnsDirective>
+        <Inject
+          services={[
+            Resize,
+            Sort,
+            ContextMenu,
+            Filter,
+            Page,
+            ExcelExport,
+            Edit,
+            PdfExport,
+          ]}
+        />
+      </GridComponent>
+    </div>
+  );
+};
 
-export default Orders
+export default Orders;
